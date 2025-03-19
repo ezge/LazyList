6ce53633720b026724956a87f2560bd2bd1d6c70
ezge <ezge.rea@gmail.com>
2025-03-19 08:43:33 +0100

    lazylist

--- app/src/main/java/com/assessment/lazylist/MainActivity.kt
+++ app/src/main/java/com/assessment/lazylist/MainActivity.kt
@@ -12,18 +12,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.assessment.lazylist.ui.theme.LazyListTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.*
 
 class MainActivity : ComponentActivity() {
+    private var itemArray: Array<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        itemArray = resources.getStringArray(R.array.car_array)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LazyListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(itemArray = itemArray as Array<out String>,
+                               modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +46,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen( itemArray: Array<out String>, modifier: Modifier = Modifier) {
+    //ImageLoader("Plymouth GTX")
+    //MyListItem("Buick Roadmaster")
+
+    val context = LocalContext.current
+    val onListItemClick = { text : String ->
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+    LazyColumn(modifier) {
+        items(itemArray) { model ->
+            MyListItem(item = model, onItemClick = onListItemClick)
+        }
+    }
+}
+
+@Composable
+fun MyListItem(item: String, onItemClick: (String) -> Unit) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { onItemClick(item) },
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    )
+    {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            ImageLoader(item)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = item,
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageLoader(item: String) {
+    val url = "https://www.ebookfrenzy.com/book_examples/car_logos/" +
+            item.substringBefore(" ") + "_logo.png"
+
+    Image(
+        painter = rememberAsyncImagePainter(url),
+        contentDescription = "car image",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.size(75.dp)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val itemArray: Array<String> = arrayOf("Cadillac Eldorado", "Ford Fairlane", "Plymouth Fury")
     LazyListTheme {
-        Greeting("Android")
+        MainScreen( itemArray = itemArray )
     }
 }
